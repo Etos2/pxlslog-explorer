6013f967e2cda3bf3c538b4fdfc04e3642db83ca
Etos2 <etos2etos2@gmail.com>
2022-05-24 08:41:18 +1000

    Merge shenanigans

--- src/parser.rs
+++ src/parser.rs
@@ -1,52 +1,6 @@
 use std::io::{self, Read};
 
 use rayon::prelude::*;
-<<<<<<< Updated upstream
-=======
-use serde_json::Value;
-
-// TODO: Line numbers for errors
-#[non_exhaustive]
-#[derive(Debug)]
-pub enum ParserError {
-    Io(io::Error),
-    Eof(),
-    BadToken(String),
-    BadByte(u8),
-    Unsupported(),
-}
-
-impl error::Error for ParserError {}
-
-impl std::fmt::Display for ParserError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            ParserError::Io(err) => write!(f, "{}", err),
-            ParserError::Eof() => write!(f, "unexpected eof"),
-            ParserError::BadToken(s) => write!(f, "invalid token ({})", s),
-            ParserError::BadByte(b) => write!(f, "invalid byte ({})", b),
-            ParserError::Unsupported() => write!(f, "unsupported format"),
-        }
-    }
-}
-
-impl From<std::io::Error> for ParserError {
-    fn from(value: std::io::Error) -> Self {
-        Self::Io(value)
-    }
-}
-
-impl From<serde_json::Error> for ParserError {
-    fn from(value: serde_json::Error) -> Self {
-        Self::BadToken(value.to_string())
-    }
-}
-impl From<hex::FromHexError> for ParserError {
-    fn from(value: hex::FromHexError) -> Self {
-        Self::BadToken(value.to_string())
-    }
-}
->>>>>>> Stashed changes
 
 // TODO: impl From
 pub struct PxlsParser {}
